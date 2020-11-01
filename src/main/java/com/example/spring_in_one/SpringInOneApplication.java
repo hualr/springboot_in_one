36c0c75ae2bd96eb55319643156a3b9bab944201
zzongqi <3031720178@qq.com>
2020-11-01 17:25:06 +0800

    Initial commit

---
+++ src/main/java/com/example/spring_in_one/SpringInOneApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_in_one;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringInOneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringInOneApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/spring_in_one/SpringInOneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_in_one;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringInOneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

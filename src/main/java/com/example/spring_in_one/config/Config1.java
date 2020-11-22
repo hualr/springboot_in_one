9001e0e5a8e38ae2c2a4039d5c98533fb9c01c9f
zzongqi <3031720178@qq.com>
2020-11-22 20:02:47 +0800

    读取配置2: 批量注入属性

---
+++ src/main/java/com/example/spring_in_one/config/Config1.java
@@ -0,0 +1,45 @@
+package com.example.spring_in_one.config;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+/**
+ * Author: zongqi
+ * Function:
+ * Creating Time：2020/11/22 15:28
+ * Version: 1.0.0
+ */
+
+/**
+ * 这个注解主要是提供自定义属性 在完成pom配置后可以在属性中更加方便的批量注入自己想要的信息
+ */
+@ConfigurationProperties(prefix = "com.example.config")
+
+@Component
+public class Config1 {
+    /**
+     * 可以在未配置的情况下提供默认值
+     */
+    private String name="LiAn";
+    private Integer age=16;
+
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * 必须设置set属性 从而实现属性的注入
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+}
--- src/main/java/com/example/spring_in_one/controller/TestController.java
+++ src/main/java/com/example/spring_in_one/controller/TestController.java
@@ -1,5 +1,7 @@
 package com.example.spring_in_one.controller;
 
+import com.example.spring_in_one.config.Config1;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,12 +22,21 @@ public class TestController {
     @Value("${num.num2}")
     private String num2;
 
+    /**
+     * 要想获取值 必须注入的方式 而非new出来对象
+     */
+    @Autowired
+    private Config1 config1;
+
 
     @RequestMapping("test")
     @ResponseBody
     public String test() {
         System.out.println(num1);
         System.out.println(num2);
+        System.out.println(new Config1().getAge());
+        System.out.println(config1.getAge());
+        System.out.println(config1.getName());
         return "OK";
     }
 

71baf083f764f509b043ab17525193a8da7e79cf
宗旗 <qzong@fiberhome.com>
2020-11-29 21:45:44 +0800

    netty参数说明 之后很长一段时间内都不会搞netty 因为我目前还搞不清楚netty有什么非常好用的功能

--- src/main/java/com/example/spring_in_one/server/NettyServer.java
+++ src/main/java/com/example/spring_in_one/server/NettyServer.java
@@ -25,6 +25,24 @@ public class NettyServer {
         EventLoopGroup bossGroup = new NioEventLoopGroup(1);
         //new 一个工作线程组
         EventLoopGroup workGroup = new NioEventLoopGroup(200);
+
+        /**
+         * ChannelOption.SO_BACKLOG 对应tcpIP协议中listene函数中的backlog参数 用于初始化服务端可连接的队列书
+         *服务端处理客户端请求是顺序进行的 因此同一事件只能处理一个客户端的连接
+         * 当多个客户端来的时候,会将那些暂时处理不了的客户端请求放在一个队列中,这个参数指定了队列大小
+         *
+         *
+         * ChannelOption.SO_REUSRTADDTR 对应socket中对应字段,表示允许重复使用本地治和端口
+         * 比如,进程中的80端口已经被某个服务器给用来监听了,此时一个服务器尝试以该端口进行监听就会返回错误,该参数允许服务器公用该端口
+         * 常用的场景是,某个进程非正常的退出了,此时端口可能会被占用很长一段时间才可以被其他进程所使用.此时想要恢复连接,就得设置该端口
+         *
+         * SO_KEEPALIVE 参数对应套接字中的对应字段 用于设置tcp连接,当发现一个tcp连接长时间没有数据通信,就会自动发送一个活动探测的数据报文
+         *
+         * SO_SNDBUFFE SO_RCVBUF SO_用来操作接收和发送缓冲区的大小 接收缓冲区用来保存网络协议站内收到的数据,直到该引用程序读取成功
+         * 发送缓冲区用来保存发送的数据,直到发送成功
+         *
+         *
+         */
         ServerBootstrap bootstrap = new ServerBootstrap()
                 .group(bossGroup, workGroup)
                 .channel(NioServerSocketChannel.class)
@@ -38,6 +56,11 @@ public class NettyServer {
         try {
             ChannelFuture future = bootstrap.bind(socketAddress).sync();
             log.info("服务器启动开始监听端口: {}", socketAddress.getPort());
+            /**
+             * closeFuture表示开启了一个channel监听器,用来监听channel是否处于关闭状态
+             * 当处于关闭状态,子线程才会被关闭(子线程才是真诚进行监听和接收请求的)
+             * syncUninterruptibly让主线程同步等待子线程结束
+             */
             future.channel().closeFuture().sync();
         } catch (InterruptedException e) {
             e.printStackTrace();
